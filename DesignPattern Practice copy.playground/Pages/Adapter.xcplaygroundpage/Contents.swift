5c9dc0e5224812d4cabc850a43b07d45f627b857
afrench53198 <afrench53198@gmail.com>
2017-10-19 18:25:19 -0700

    Adapter implemented

--- DesignPattern Practice copy.playground/Pages/Adapter.xcplaygroundpage/Contents.swift
+++ DesignPattern Practice copy.playground/Pages/Adapter.xcplaygroundpage/Contents.swift
@@ -1,7 +1,51 @@
 //: [Previous](@previous)
 
 import Foundation
+// The Adapter Pattern is when you take an interface "x" and using a class or a common interface "adapt" interface x to interface "y". In this example, square is being adapted to the Rectangle protocol.
 
-var str = "Hello, playground"
+class Square
+{
+    var side = 0
+    
+    init(side: Int)
+    {
+        self.side = side
+    }
+}
+
+protocol Rectangle
+{
+    var width: Int { get }
+    var height: Int { get }
+}
+
+extension Rectangle
+{
+    var area: Int
+    {
+        return self.width * self.height
+    }
+}
+
+class SquareToRectangleAdapter : Rectangle
+{
+    var width: Int
+    var height: Int
+    
+    init(_ square: Square)
+    {
+        self.width = square.side
+        self.height = square.side
+    }
+    func toSquare(rect: Rectangle) -> Square? {
+        if rect.width == rect.height {
+            return Square(side: width)
+        } else {
+            return nil
+        }
+    }
+}
+//: [Next](@next)
+
 
 //: [Next](@next)
